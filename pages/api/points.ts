e6e3c7608c0c21b653c7b65e5fcaeb5571cdaf4b
george <7jagjag@gmail.com>
2022-11-22 13:57:18 +0100

    fix leaderboard

--- pages/api/points.ts
+++ pages/api/points.ts
@@ -83,7 +83,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     }
     // byId[id].bets.push(bet)
     if (byMatchNumber[bet.match_number]) {
-      byId[id].points = calcPoints(byMatchNumber[bet.match_number], bet)
+      byId[id].points += calcPoints(byMatchNumber[bet.match_number], bet)
     }
 
     return byId
