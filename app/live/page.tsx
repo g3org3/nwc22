bfc77b534ecf9276a72397a7b2a1844f89e121f0
george <7jagjag@gmail.com>
2022-11-22 12:46:42 +0100

    load live video

---
+++ app/live/page.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  //
+}
+
+const Live = (props: Props) => {
+  return (
+    <>
+      <video
+        data-html5-video=""
+        preload="metadata"
+        src="blob:https://utopianwilderness.com/fd05b06c-32df-44a5-a44e-495d13e6e817"
+      ></video>
+    </>
+  )
+}
+
+export default Live
